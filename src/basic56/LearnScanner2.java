0c83b9c5ad95bd8f26e812190c3a11ef9f95efd6
sharifurbg <sharifurbg@gmail.com>
2020-09-10 20:11:08 -0500

    something updated

--- src/basic56/LearnScanner2.java
+++ src/basic56/LearnScanner2.java
@@ -3,33 +3,13 @@ package basic56;
 import java.util.Scanner;
 
 public class LearnScanner2 {
-public static void main(String[]args) {
-System.out.println("Enter your name");
+	public static void main(String[] args) {
+		System.out.println("Enter your name");
 //LearnScanner object=new LearnScanner();
-Scanner input=new Scanner(System.in);	
-System.out.println("Enter your age");	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your age");
+		System.out.println("Hello");
+
+	}
+
 }
