182a18f58bd9c86e3b043c1b9d7e80675c6fbcba
sharifurbg <sharifurbg@gmail.com>
2020-09-10 19:55:49 -0500

    First commit

---
+++ src/basic56/LearnScanner2.java
@@ -0,0 +1,35 @@
+package basic56;
+
+import java.util.Scanner;
+
+public class LearnScanner2 {
+public static void main(String[]args) {
+System.out.println("Enter your name");
+//LearnScanner object=new LearnScanner();
+Scanner input=new Scanner(System.in);	
+System.out.println("Enter your age");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
